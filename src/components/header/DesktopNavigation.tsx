11018bdf674b6107be861bb95c50725c148f59cf
Cheam Norakpanha <cheam.panha168@gmail.com>
2025-10-12 13:04:47 +0700

    Revert "Add EN/KH translation"
    This reverts commit d9c03bdc9b884d3d3a78e83e22bf7988bfb292c7.

--- src/components/header/DesktopNavigation.tsx
+++ src/components/header/DesktopNavigation.tsx
@@ -1,25 +1,22 @@
-import { useTranslation } from "react-i18next";
 import { Link } from "react-scroll"
 
 export default function DesktopNavigation() {
-    const [t, i18n] = useTranslation("global");
-    
     return (
         <nav className="hidden md:flex items-center space-x-8">
             <Link to="about" smooth={true} duration={500} offset={-65} className="text-md font-medium transition-colors hover:text-yellow-700 mr-0 cursor-pointer">
-                {t("navbar.about")}
+                About
             </Link>
             <Link to="team" smooth={true} duration={500} offset={-65} className="text-md font-medium transition-colors hover:text-yellow-700 cursor-pointer">
-                {t("navbar.team")}
+                Team
             </Link>
             <Link to="issue" smooth={true} duration={500} offset={-65} className="text-md font-medium transition-colors hover:text-yellow-700 cursor-pointer">
-                {t("navbar.issue")}
+                Issue
             </Link>
             <Link to="mission" smooth={true} duration={500} offset={-65} className="text-md font-medium transition-colors hover:text-yellow-700 cursor-pointer">
-                {t("navbar.mission")}
+                Mission
             </Link>
             <Link to="contact" smooth={true} duration={500} offset={-65} className="text-md font-medium transition-colors hover:text-yellow-700 cursor-pointer">
-                {t("navbar.contact")}
+                Contact
             </Link>
         </nav>
     )
--- src/components/header/GetInvolvedButton.tsx
+++ src/components/header/GetInvolvedButton.tsx
@@ -1,13 +1,10 @@
-import { useTranslation } from "react-i18next";
 import { Link } from "react-scroll";
 
 export default function GetInvolvedButton() {
-    const [t, i18n] = useTranslation("global");
-    
     return (
         <div className="hidden md:flex items-center">
             <Link to="get-involved" smooth={true} duration={500} offset={-65}>
-                <button className="border-yellow-700 bg-yellow-700 text-white hover:bg-yellow-800 px-6 py-2 rounded-md font-medium transition-colors">{t("navbar.getInvolved")}</button>
+                <button className="border-yellow-700 bg-yellow-700 text-white hover:bg-yellow-800 px-6 py-2 rounded-md font-medium transition-colors">Get Involved</button>
             </Link>
         </div>
     );

d9c03bdc9b884d3d3a78e83e22bf7988bfb292c7
Cheam Norakpanha <cheam.panha168@gmail.com>
2025-10-12 11:58:55 +0700

    Add EN/KH translation

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,30 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import global_en from './translations/en/global.json';
+import global_kh from './translations/kh/global.json';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en
+    },
+    kh: {
+      global: global_kh
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

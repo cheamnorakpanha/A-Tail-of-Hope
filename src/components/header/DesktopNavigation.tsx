6757d8e6d0f6a3625ed86efd7a5f73d4a17a2938
Cheam Norakpanha <cheam.panha168@gmail.com>
2025-10-16 15:41:52 +0700

    Change English font

--- src/components/header/DesktopNavigation.tsx
+++ src/components/header/DesktopNavigation.tsx
@@ -6,7 +6,7 @@ export default function DesktopNavigation() {
     const { t } = useLanguage();  // t stands for Translations
 
     return (
-        <nav className="hidden md:flex items-center space-x-8">
+        <nav className="hidden md:flex items-center space-x-12">
             <Link to="about" smooth={true} duration={500} offset={-65} className="text-md font-semibold transition-colors hover:text-yellow-700 mr-0 cursor-pointer">
                 {t.navbar.about}
             </Link>
